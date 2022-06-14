c7bb0ed082c97ea9573c29018b63df693bb4b972
Nirveek21 <nirveek21@gmail.com>
2022-06-14 21:02:04 +0530

    added second tittle

---
+++ src/Elements/Tittle2.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Tittle2 = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&family=Luckiest+Guy&display=swap");
+  color: #39ff14;
+  font-size: 50px;
+  font-family: "Coda Caption", sans-serif;
+  font-family: "Luckiest Guy", cursive;
+  margin-bottom: 2%;
+`;
+export default Tittle2;
--- src/components/Mode.js
+++ src/components/Mode.js
@@ -2,11 +2,12 @@ import React from "react";
 import Title from "../Elements/Title";
 import styled from "styled-components";
 import GreenButton from "../Elements/GreenButton";
-
+import Tittle2 from "../Elements/Tittle2";
 function Mode({ clickHumanBtn, clickAIBtn }) {
   return (
     <>
       <Title>Tic Tac Toe</Title>
+      <Tittle2>Win if you can</Tittle2>
       <GreenButton onClick={clickHumanBtn}>Player VS Player</GreenButton>
       <GreenButton onClick={clickAIBtn}>Player VS AI</GreenButton>
     </>
